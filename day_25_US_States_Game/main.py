153c090bb39ff99f87bdf55e506435b9059f94bc
bluescreenlife <bluescreenlife91@gmail.com>
2023-10-26 12:49:17 -0500

    fixed end csv creation, added comments

--- day_25_US_States_Game/main.py
+++ day_25_US_States_Game/main.py
@@ -1,55 +1,34 @@
+'''US States guessing/labeling game'''
 import turtle
 import pandas
 
+# set up screen with US map
 screen = turtle.Screen()
 screen.title("U.S. States Game")
-image = 'blank_states_img.gif'
+image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-class StatesGame:
-    def __init__(self):
-        self.states_df = pandas.read_csv('50_states.csv')
-        self.states_list = self.states_df.to_dict('records')
-        self.score = 0
+# get state names and coordiantes for map
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
 
-        self.labeler = turtle.Turtle()
-        self.labeler.hideturtle()
-        self.labeler.penup()
-        self.labeler.speed('fastest')
-
-        self.game_on = True
-
-    def get_state_data(self):
-        state_entry = screen.textinput(title=f"Guess The State! Score: {self.score}", prompt= "Enter a state's name:").title()
-
-        if state_entry == "exit":
-            self.game_on = False
-            return None
-        
-        for state_dict in self.states_list:
-            if state_dict['state'] == state_entry:
-                coordinates = (state_dict['x'], state_dict['y'])
-                self.states_list.remove(state_dict)
-                return state_entry, coordinates
-
-    def label_state(self, state, coords):
-        self.labeler.goto(coords)
-        self.labeler.write(state)
-        self.score += 1
-
-    def run_game(self):
-        while self.game_on == True:
-            while len(self.states_list) > 0:
-                state_to_write, coords_to_write = self.get_state_data()
-                self.label_state(state_to_write, coords_to_write)
-
-    def end_report(self):
-        remaining_states = pandas.DataFrame.from_dict(self.states_list)
-        remaining_states.to_csv('./states_to_study.csv')
-
-game = StatesGame()
-game.run_game()
-game.end_report()
-
-screen.exitonclick()
+# loop to take user input, if match state, print state name at coordinates on map
+# user types exit to quit, remaining states are exported to csv as states to study
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What's another state's name?").title()
+    if answer_state == "Exit":
+        missing_states = [state for state in all_states if state not in guessed_states]
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
