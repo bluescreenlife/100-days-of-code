04f63c52e5158c103b06987c672e06408dc58de8
bluescreenlife <bluescreenlife91@gmail.com>
2023-12-19 19:28:32 -0600

    Fixed chromedriver issue, now fully operational.

--- day_52_instagram_follower_bot/main.py
+++ day_52_instagram_follower_bot/main.py
@@ -1,26 +1,29 @@
-'''Logs into an Instagram account, gets follower list of another account, and follows as many as possible.'''
+'''Logs into an Instagram account, gets follower list of another account, and follows as many as possible.
+Current viewable follower list for any account is 50 accounts, therefore this is the max this program can follow.
+
+Instagram now seems to have an invisible overlay element over the followers element 
+to prevent bot clicks = this program is not currently operational for this reason.'''
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
-from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
 import os
 from dotenv import load_dotenv
-from random import randint
 
+CHROMEDRIVER_PATH = ChromeDriverManager().install()
+
+# load login credentials
 load_dotenv()
-
-CHROMEDRIVER_PATH = "/Users/andrew/Developer/chromedriver" # set to your local chromedriver path
-
-# fetched from local .env file
 IG_USERNAME = os.environ.get("IG_USERNAME")
 IG_PW = os.environ.get("IG_PW")
-IG_TARGET_ACCT = "https://www.instagram.com/vinylmeplease/" # set to account for which you want to follow their followers
+IG_TARGET_ACCT = "https://www.instagram.com/vinylmeplease/followers" # set to account for which you want to follow their followers
 
 class InstaFollower:
+    '''Logs into Instagram and follows all accounts of a specified account.'''
     def __init__(self, driver_path:str, username:str, password:str):
         self.service = Service(executable_path=driver_path)
         self.chrome_options = webdriver.ChromeOptions()
@@ -33,59 +36,60 @@ class InstaFollower:
         self.accounts_to_follow = []
 
     def login(self):
-        # wait_time_short = randint(1, 5)
-        # wait_time_long = randint(10, 20)
+        '''Logs in to Instagram, clears prompts.'''
         self.driver.get("https://www.instagram.com/")
-        sleep(16.1)
+        sleep(3)
 
+        # log in
         username_input = self.driver.find_element(By.XPATH, "//*[@id='loginForm']/div/div[1]/div/label/input")
         username_input.send_keys(self.username)
-        sleep(1.2)
+        sleep(1)
         
         pw_input = self.driver.find_element(By.XPATH, "//*[@id='loginForm']/div/div[2]/div/label/input")
         pw_input.send_keys(self.pw)
         pw_input.send_keys(Keys.ENTER)
-        
-        # IG seems to detect bot here and close browser if code run too many times
-        
-        sleep(16.2) # wait while page loads
+        sleep(3)
         
         # dismiss save login info prompt
         save_login_not_now = self.driver.find_element(By.XPATH, "//div[contains(text(), 'Not now')]")
         if save_login_not_now:
             save_login_not_now.click()
-        sleep(3.2)
+        sleep(3)
 
         # dismiss notifications prompt
-        notifications_not_now = self.driver.find_element(By.XPATH, "//button[contains(text(), 'Not now')]")
-        if notifications_not_now:
+        try:
+            notifications_not_now = self.driver.find_element(By.XPATH, "//button[contains(text(), 'Not now')]")
             notifications_not_now.click()
-
-        print("Login complete. Waiting while page loads...")
-        sleep(12.2) # wait while page loads
+        except NoSuchElementException:
+            try:
+                notifications_not_now = self.driver.find_element(By.XPATH, "/html/body/div[3]/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]")
+                notifications_not_now.click()
+            except NoSuchElementException:
+                pass
+        sleep(3)
 
     def find_followers(self, target_account:str):
+        '''Retrieves list of follower elements.'''
+        sleep(5)
         self.driver.get(target_account)
-        sleep(10.7) # wait while page loasd
+        
+        self.accounts_to_follow = self.driver.find_elements(By.CLASS_NAME, "x1dm5mii x16mil14 xiojian x1yutycm x1lliihq x193iq5w xh8yej3")
+        if self.accounts_to_follow:
+            print(f"Located {len(self.accounts_to_follow)} followable accounts.")
 
-        followers = self.driver.find_element(By.XPATH, "//*[@id='mount_0_0_g1']/div/div/div[2]/div/div/div[1]/div[2]/section/main/div/header/section/ul/li[2]/button")
-        followers.click()
-        sleep(11.2)
-
-        followers_list_element = self.driver.find_element(By.XPATH, "/html/body/div[6]/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]/div[2]")
-        followers_list = followers_list_element.find_elements(By.TAG_NAME, "div")
-        self.accounts_to_follow = followers_list
-        print(f"Number of followers found: {len(followers_list)}")
-       
+        # remove any already-followed accounts
+        for account in self.accounts_to_follow:
+            if account.find_element(By.CLASS_NAME, "_ap3a _aaco _aacw _aad6 _aade").text == "Requested" or "Following":
+                self.accounts_to_follow.remove(account)
+        
     def follow(self):
-        for follower in self.accounts_to_follow:
-            wait_time = randint(1, 3)
-            sleep(wait_time)
-            follow_button = follower.find_element(By.CLASS_NAME, " _acan _acap _acas _aj1- _ap30")
-            if follow_button:
-                follow_button.click()
-                print("Followed 1 account.")
-        print(f"Successfully followed all accounts.")
+        '''Clicks follow button on each follower element'''
+        for account in self.accounts_to_follow:
+            follow_button = account.find_element(By.CLASS_NAME, "_ap3a _aaco _aacw _aad6 _aade")
+            follow_button.click()
+            print("Follow confirmed.")
+        print(f"Successfully followed all accounts. Quitting browser in 5s...")
+        self.driver.close()
 
 
 if __name__ == "__main__":
