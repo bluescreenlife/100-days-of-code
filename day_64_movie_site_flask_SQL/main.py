81a9d7d865452d4862a0feee11f28e99fd7e1442
bluescreenlife <bluescreenlife91@gmail.com>
2024-01-25 17:22:25 -0600

    completed project

--- day_64_movie_site_flask_SQL/main.py
+++ day_64_movie_site_flask_SQL/main.py
@@ -1,8 +1,6 @@
-from crypt import methods
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
-from h11 import Data
 from numpy import integer
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Float, null
@@ -28,13 +26,13 @@ db.init_app(app)
 # CREATE TABLE MODEL
 class Movie(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    title: Mapped[str] = mapped_column(String(25), unique=True, nullable=False)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
     year: Mapped[int] = mapped_column(Integer, nullable=False)
-    description: Mapped[str] = mapped_column(String, nullable=False)
-    rating: Mapped[float] = mapped_column(Float, nullable=False)
-    ranking: Mapped[integer] = mapped_column(Integer, nullable=False)
-    review: Mapped[str] = mapped_column(String, nullable=False)
-    img_url: Mapped[str] = mapped_column(String, nullable=False)
+    description: Mapped[str] = mapped_column(String(500), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=True)
+    ranking: Mapped[integer] = mapped_column(Integer, nullable=True)
+    review: Mapped[str] = mapped_column(String(250), nullable=True)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
 
 
 # CREATE TABLE SCHEMA IN DB
@@ -43,19 +41,26 @@ with app.app_context():
 
 # WTForm templates
 class UpdateForm(FlaskForm):
-    updated_rating = StringField(label="New Rating")
-    updated_review = StringField(label="New Review")
+    updated_rating = StringField(label="Your Rating:")
+    updated_review = StringField(label="Your Review:")
     submit = SubmitField("Update Movie")
 
 class AddForm(FlaskForm):
     movie_title = StringField(label="Movie Title")
     submit = SubmitField("Add Movie")
-
+    
+# Flask routes
+    
 @app.route("/")
 def home():
 
     result = db.session.execute(db.select(Movie).order_by(Movie.rating))
-    all_movies = result.scalars()
+    all_movies = result.scalars().all()
+
+    # assign movie.ranking to its position in the list
+    for i in range(len(all_movies)):
+        all_movies[i].ranking = len(all_movies) - i
+    db.session.commit()
 
     return render_template("index.html", movies=all_movies)
 
@@ -67,7 +72,7 @@ def add():
         user_search = add_form.movie_title.data
         formatted_search = quote(user_search)
         
-        API_KEY = "5ffa087d721280239f790e5b12c35a7a"
+        # API_KEY = "5ffa087d721280239f790e5b12c35a7a"
         URL = f"https://api.themoviedb.org/3/search/movie?query={formatted_search}&include_adult=true&language=en-US&page=1"
 
         headers = {
@@ -75,18 +80,46 @@ def add():
             "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZmZhMDg3ZDcyMTI4MDIzOWY3OTBlNWIxMmMzNWE3YSIsInN1YiI6IjY1YjE4N2ViM2M0MzQ0MDE5MzY2NWEwNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XrpR9fQ53p5hXs9o8_jYP1Eu_meEvqpOXCHNqCaPC9U"
         } 
 
-        top_5_results_ids = []
-
         response = requests.get(URL, headers=headers)
         if response.status_code == 200:
-            movie_results = response.json()["results"][:4] # top 5 movie results
-            print(f"\n{movie_results}\n")
-            top_5_results_ids = []
+            data = response.json()
+            movie_search_data = [movie for movie in data["results"][:9]]
 
-
-
+            if movie_search_data:
+                return render_template("select.html", data=movie_search_data)
+            else:
+                print("Failed to retrieve movie search data.")
 
     return render_template("add.html", form=add_form)
+
+@app.route("/find")
+def find():
+    selection_id = request.args.get("id")
+    print(f"Retrieved selection id: {selection_id}")
+
+    # lookup movie details
+    URL = f"https://api.themoviedb.org/3/movie/{selection_id}?language=en-US"
+
+    headers = {
+        "accept": "application/json",
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZmZhMDg3ZDcyMTI4MDIzOWY3OTBlNWIxMmMzNWE3YSIsInN1YiI6IjY1YjE4N2ViM2M0MzQ0MDE5MzY2NWEwNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XrpR9fQ53p5hXs9o8_jYP1Eu_meEvqpOXCHNqCaPC9U"
+    } 
+
+    response = requests.get(URL, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        # print(data)
+    else:
+        print(f"\nData retrieval error: {response.status_code}\n")
+
+    # add movie to database
+    movie_to_add = Movie(title=data["original_title"], img_url=f"https://image.tmdb.org/t/p/original{data['poster_path']}", year=data['release_date'].split("-")[0], description=data['overview'])
+    
+    db.session.add(movie_to_add)
+    db.session.commit()
+
+    # redirect to home
+    return redirect(url_for("update", id=movie_to_add.id))
 
 @app.route("/update", methods=["GET", "POST"])
 def update():
