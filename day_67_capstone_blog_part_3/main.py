05b8520c3024e7a311944979ff7e6fcb79037b43
bluescreenlife <bluescreenlife91@gmail.com>
2024-02-04 20:32:08 -0600

    initial commit

---
+++ day_67_capstone_blog_part_3/main.py
@@ -0,0 +1,94 @@
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Text
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, URLField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditor, CKEditorField
+from datetime import date
+
+app = Flask(__name__)
+ckeditor = CKEditor(app)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap5(app)
+
+# CREATE DATABASE
+class Base(DeclarativeBase):
+    pass
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+
+# CONFIGURE TABLE
+class BlogPost(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    subtitle: Mapped[str] = mapped_column(String(250), nullable=False)
+    date: Mapped[str] = mapped_column(String(250), nullable=False)
+    body: Mapped[str] = mapped_column(Text, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
+
+# CONFIGURE NEW POST FORM
+class PostForm(FlaskForm):
+    title = StringField(label="Post Title")
+    subtitle = StringField(label="Post Subtitle")
+    author = StringField(label="Author Name")
+    bg_img_url = URLField(label="Background Image URL")
+    body = CKEditorField(label="Body")
+    submit = SubmitField("Create Post")
+
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/')
+def get_all_posts():
+    result = db.session.execute(db.select(BlogPost))
+    posts = result.scalars()
+    return render_template("index.html", all_posts=posts)
+
+@app.route('/<int:post_id>')
+def show_post(post_id):
+    requested_post = db.session.execute(db.select(BlogPost).where(BlogPost.id == post_id)).scalar()
+    return render_template("post.html", post=requested_post)
+
+
+# TODO: add_new_post() to create a new blog post
+@app.route('/new-post')
+def add_new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        new_post = BlogPost(title = form.title.data,
+                            subtitle = form.subtitle.data,
+                            date = date.today,
+                            body = form.body.data,
+                            author = form.author.data,
+                            img_url = form.bg_img_url.data)
+        db.session.add(new_post)
+        db.session.commit()
+        
+
+    return render_template("make-post.html", form=form)
+
+# TODO: edit_post() to change an existing blog post
+
+# TODO: delete_post() to remove a blog post from the database
+
+# Below is the code from previous lessons. No changes needed.
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5003)
