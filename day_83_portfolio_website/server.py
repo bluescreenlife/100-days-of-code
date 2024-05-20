c2aa941c94d1031e457997cc21747ac927dc7bb8
bluescreenlife <bluescreenlife91@gmail.com>
2024-05-20 11:44:06 -0500

    add route for form data

--- day_83_portfolio_website/server.py
+++ day_83_portfolio_website/server.py
@@ -1,5 +1,5 @@
 '''A Flask web server for a developer portfolio website.'''
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
@@ -7,7 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/sendmsg")
+def sendmsg():
 
+    return redirect('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
