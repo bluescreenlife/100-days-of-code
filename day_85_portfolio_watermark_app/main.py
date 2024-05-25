b4e874f13844c1ec7e6b31dab5932fd6e58a3481
bluescreenlife <bluescreenlife91@gmail.com>
2024-05-24 21:26:48 -0500

    add canvas preview image functionality

--- day_85_portfolio_watermark_app/main.py
+++ day_85_portfolio_watermark_app/main.py
@@ -1,13 +1,15 @@
 '''A simple GUI app for watermarking images.'''
-from tkinter import PhotoImage, Tk
-from tkinter import ttk, filedialog
-import PIL
+from tkinter import Tk, ttk, Canvas, filedialog
+from PIL import Image, ImageTk
 
 image_to_mark = None # to be assigned PhotoImage object
 watermark = None # to be assigned PhotoImage object
+preview_size = (400, 400)
+image_preview = None
 
 def get_image():
     global image_to_mark
+    global image_preview
     file_path = filedialog.askopenfilename(
         title='Select an Image',
         filetypes=(('JPG', '*.jpg'), ('JPEG', '*.jpeg'), ('PNG', '*.png'), ('GIF', '.gif'))
@@ -16,36 +18,26 @@ def get_image():
     if file_path:
         print(f'Image path obtained: {file_path}')
 
-        # update image window label to display this photo
-        image_to_mark = PhotoImage(file=file_path)
-        image_label.config(image=image_to_mark)
-        image_label['image'] = image_to_mark
+        # open file, create thumbnail copy and display in preview frame 
+        image_to_mark = Image.open(file_path) # create a PIL image from file
+        image_preview = image_to_mark.copy() # create a copy for thumbnail
+        image_preview.thumbnail(preview_size) # use PIL to modify to fit in canvas dimensions
+        image_preview = ImageTk.PhotoImage(image_preview) # convert to TK image
+        preview_canvas.create_image(200, 200, image=image_preview) # place image on preview canvas
 
 def get_watermark():
-    global watermark
-    file_path = filedialog.askopenfilename(
-        title='Select a Watermark',
-        filetypes=(('JPG', '*.jpg'), ('JPEG', '*.jpeg'), ('PNG', '*.png'), ('GIF', '.gif'))
-    )
-
-    if file_path:
-        print(f'Watermark path obtained: {file_path}')
-
-        # update image window label to display this photo
-        watermark = PhotoImage(file=file_path)
-        wm_label.config(image=watermark)
-        wm_label['image'] = watermark
+    pass
 
 
 app = Tk()
 app.title('WaterMarkIt')
 
 # top frame for upload-images buttons and labels
-upload_frame = ttk.Frame(app, width=200, height=100)
+upload_frame = ttk.Frame(app, width=400, height=100)
 upload_frame.grid(row=0, column=1, columnspan=3, pady=10)
 
 # bottom frame for displaying image
-preview_frame = ttk.Frame(app, width=400, height=200, padding=20, borderwidth=5, relief='sunken')
+preview_frame = ttk.Frame(app, width=400, height=400, padding=20, borderwidth=5, relief='sunken')
 preview_frame.grid(row=1, column=1, columnspan=3)
 
 # upload image label and button
@@ -57,9 +49,8 @@ ttk.Label(upload_frame, text='Upload Watermark:').grid(row=1, column=0, padx=10)
 ttk.Button(upload_frame, text='Browse', command=get_watermark).grid(row=1, column=1)
 
 # labels for displaying photo and watermark
-image_label = ttk.Label(preview_frame)
-wm_label = ttk.Label(preview_frame)
-
+preview_canvas = Canvas(preview_frame, width=400, height=400)
+preview_canvas.pack(expand=True)
 
 
 app.mainloop()
